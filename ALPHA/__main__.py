d8cc900778a244a0f798e856f48b89ec57a69fe6
Devendra <81507524+RoyalDevendra@users.noreply.github.com>
2021-04-22 11:14:46 +0530

    Create __main__.py

---
+++ ALPHA/__main__.py
@@ -0,0 +1,11 @@
+# AlphaX Userbot - 
+
+# Copyright (C) 2021 AlphaX
+
+#
+
+# This file is a part of < https://github.com/TheAlphaX/alphax/ >
+
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TheAlphaX/AlphaX/blob/main/LICENSE>.
+
